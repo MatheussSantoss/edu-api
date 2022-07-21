ea547148173bb79a90f17869a24cb89fa227cad4
Matheus Santos <matheusOS2006@outlook.com>
2022-07-21 20:40:02 -0300

    21/07 - Adiciona configuração Heroku

--- src/server.js
+++ src/server.js
@@ -5,9 +5,15 @@ const PORT = 6666;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: true
+}));
+
+app.options('*', cors());
+
 app.use(express.json());
-app.use(routes)
-app.listen(PORT, () => {
+app.use(routes);
+app.listen(process.env.PORT || PORT, () => {
   console.log(`API education is on ${PORT}`);
 })
